10bc3ca4817dfd0d62741022f98e6685b5bc6916
haileychoii <jenny876517@gmail.com>
2023-04-15 08:37:43 +0900

    lab01_qna

--- lab_01.py
+++ lab_01.py
@@ -36,3 +36,18 @@ class Board:
 
     def __str__(self):
         return f"Board: {self.name}\nNumber of Posts: {len(self.posts)}"
+
+# 2. QnA Board, free Board 객체 생성
+
+qna = Board('QnA Board')
+free = Board('free Board')
+
+
+q1 = Post('객체지향 원리', '홍길동','객체지향의 핵심 원리 중 추상화에 대해 구체적인 예를 들어 설명해 주세요.')
+q2 = Post('파이썬 언어', '홍길순', '파이썬 언어가 다른 프로그래밍 언어에 비해 보편화된 이유가 무엇인가요?')
+
+qna.add_post(q1)
+qna.add_post(q2)
+
+for post in qna.posts:
+    print(post, '\n')
