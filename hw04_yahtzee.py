e9c72a283ec41163d9867361306133c3245a77d5
haileychoii <jenny876517@gmail.com>
2023-05-28 14:18:24 +0900

    과제중

--- hw04_yahtzee.py
+++ hw04_yahtzee.py
@@ -1,12 +1,18 @@
 # Yahtzee Game
+
 import random
 # Die class
-class Die():
-    def __init__(self, face):
-        self.face = _face
+class Die:
+    def __init__(self):
+        self.face = None
 
-    @property
-    def face(self):
-        return random.randint(1,6)
+    def roll(self):
+        self.face = random.randint(1, 6)
 
-    def roll(self):  None
+    def __str__(self):
+        return f'Dice face : {self.face}'
+
+# Yahtzee Class
+class Yahtzee:
+    def __init__(self):
+        self.dice = [Die for _ in range(5)]
